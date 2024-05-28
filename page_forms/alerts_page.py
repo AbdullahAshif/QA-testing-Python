df233a1706f99cbbf0f364f243e7b90e3928d7ec
Abdullah-Al Moinur Rashid Ashif <a.moinur@a1qa.com>
2024-05-28 17:56:38 +0600

    Updated Alert Page

--- page_forms/alerts_page.py
+++ page_forms/alerts_page.py
@@ -3,20 +3,22 @@ from selenium.webdriver.common.by import By
 from browser.py_quality_services import PyQualityServices
 from forms.base_form import BaseForm
 
-class DataTablesPage(BaseForm):
-    __page_name = "Data Tables"
+
+class AlertPage(BaseForm):
+    __page_name = "JavaScript Alerts"
     _element = PyQualityServices.element_factory
-    __due_locator = (By.XPATH, "//*[@id='table1']//td[4]")
+    __click_js_alert_btn = _element.get_button(LocatorConstants.JS_ALERT_BTN_LOCATOR,
+                                               "Click for JS Alert btn")
+    __success_message_lbl = _element.get_label(
+        (By.XPATH, LocatorConstants.PRECISE_TEXT_XPATH.format("You successfully clicked an alert")),
+        "Success message text")
 
     def __init__(self):
-        super(DataTablesPage, self).__init__((By.XPATH, LocatorConstants.PRECISE_TEXT_XPATH.format(self.__page_name)),
-                                             self.__page_name)
+        super(AlertPage, self).__init__((By.XPATH, LocatorConstants.PRECISE_TEXT_XPATH.format(self.__page_name)),
+                                        self.__page_name)
 
-    def get_first_due_list(self):
-        due_list = []
-        for label in self._get_first_due_lbl_list():
-            due_list.append(label.text)
-        return due_list
+    def click_js_alert_btn(self):
+        self.__click_js_alert_btn.click()
 
-    def _get_first_due_lbl_list(self):
-        return self._element.find_elements(self.__due_locator, "due", "label")
+    def is_success_message_displayed(self):
+        return self.__success_message_lbl.state.is_displayed
